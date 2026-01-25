fe1e38e0ca3a2cc3776669b2fb58d17dbe4045f3
jos3lo89 <ganbaru743@gmail.com>
2026-01-25 15:49:44 -0500

    update: disable directories picker
    node_modules, dist, generated and migrations

--- lazyvim/lua/plugins/snacks.lua
+++ lazyvim/lua/plugins/snacks.lua
@@ -1,13 +1,23 @@
 local utils = require("utils.utils")
 
 return {
-  "folke/snacks.nvim",
-  opts = {
-    dashboard = {
-      enabled = true,
-      preset = {
-        header = utils.logo,
-      },
-    },
-  },
+	"folke/snacks.nvim",
+	opts = {
+		dashboard = {
+			enabled = true,
+			preset = {
+				header = utils.logo,
+			},
+		},
+		picker = {
+			enabled = true,
+			exclude = {
+				".git",
+				"dist",
+				"node_modules",
+				"generated",
+				"migrations",
+			},
+		},
+	},
 }
